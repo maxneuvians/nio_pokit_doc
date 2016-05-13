5ff10ab213ad941501bf7da3d69920847302522e
Max Neuvians <max@neuvians.net>
2016-05-13 15:46:30 -0400

    Fixed optional provider name in eligibility check

--- lib/nio_pokit_doc/api/eligibility.ex
+++ lib/nio_pokit_doc/api/eligibility.ex
@@ -6,6 +6,11 @@ defmodule NioPokitDoc.API.Eligibility do
       |> Parser.parse_elibility
   end
 
+  def post(%{member: %{birth_date: _, first_name: _, last_name: _, id: _}, provider: %{organization_name: _, npi: _}, trading_partner_id: _} = params) do
+    Base.request(:post, "eligibility/", params)
+      |> Parser.parse_elibility
+  end
+
   def post(_params) do
     "You need to include correct member, provider, and trading_partner_id params"
   end
