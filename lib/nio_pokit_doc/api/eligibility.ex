d090b2616165f6af91781adaf3a4bb81cce67998
Max Neuvians <max@neuvians.net>
2016-05-16 13:26:08 -0400

    Added service types

--- lib/nio_pokit_doc/api/eligibility.ex
+++ lib/nio_pokit_doc/api/eligibility.ex
@@ -1,17 +1,17 @@
 defmodule NioPokitDoc.API.Eligibility do
   alias NioPokitDoc.{API.Base, Parser}
 
-  def post(%{member: %{birth_date: _, first_name: _, last_name: _, id: _}, provider: %{first_name: _, last_name: _, npi: _}, trading_partner_id: _} = params) do
+  def post(%{member: %{birth_date: _, first_name: _, last_name: _, id: _}, provider: %{first_name: _, last_name: _, npi: _}, trading_partner_id: _, service_types: _} = params) do
     Base.request(:post, "eligibility/", params)
       |> Parser.parse_elibility
   end
 
-  def post(%{member: %{birth_date: _, first_name: _, last_name: _, id: _}, provider: %{organization_name: _, npi: _}, trading_partner_id: _} = params) do
+  def post(%{member: %{birth_date: _, first_name: _, last_name: _, id: _}, provider: %{organization_name: _, npi: _}, trading_partner_id: _, service_types: _} = params) do
     Base.request(:post, "eligibility/", params)
       |> Parser.parse_elibility
   end
 
   def post(_params) do
-    "You need to include correct member, provider, and trading_partner_id params"
+    "You need to include correct member, provider, service types, and trading_partner_id params"
   end
 end
